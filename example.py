94af7aecbb74096de30861acd0a4977eccb43f16
Nexidian <obsidian1710@gmail.com>
2019-07-21 00:35:55 +0100

    Removed old comments

--- example.py
+++ example.py
@@ -1,8 +1,5 @@
 import PornhubCommentsWordcloud
 
 app = PornhubCommentsWordcloud.PornhubCommentsWordcloud()
-app.generate_wordcloud(video_count=100, use_cache=True)
-# most_viewed_links = app.get_most_viewed_links(50, False)
-# print (len(most_viewed_links))
-# print (most_viewed_links)
 
+app.generate_wordcloud(video_count=100, use_cache=True)
